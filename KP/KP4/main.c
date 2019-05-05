3029bdad8a32f06edb03c0bba91506c68c031b6b
Sin-Denis <cdenissin@gmail.com>
2019-05-05 12:26:58 +0300

    Update makefile and main.c

--- KP/KP4/main.c
+++ KP/KP4/main.c
@@ -4,12 +4,18 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2) {
+    if (argc < 2) {
         printf("Using: ./main file\n");
         exit(1);
     }
 
     FILE *f = fopen(argv[1], "r");
+    if (f == NULL) {
+        printf("File not exist");
+        exit(1);
+    }
+    int i;
+    char *res;
     Table t;
 
     table_create(&t);
@@ -17,6 +23,16 @@ int main(int argc, char *argv[])
     table_print(&t);
     table_heap_sort(&t);
     table_print(&t);
+    i = 2;
+    while (i < argc) {
+        res = table_binary_search(&t, atoi(argv[i]));
+        if (res == NULL)
+            printf("%d: Key not found\n", atoi(argv[i]));
+        else
+            printf("%d:\n%s", atoi(argv[i]), res);
+        ++i;
+    }
     table_destroy(&t);
+    fclose(f);
     return (0);
 }
